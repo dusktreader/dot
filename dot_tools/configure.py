04a442126c2c898f5428be4ad0f675166f098d3a
dusktreader <tucker.beck@gmail.com>
2016-12-20 16:36:13 -0800

    interim commit

--- dot_tools/configure.py
+++ dot_tools/configure.py
@@ -202,6 +202,7 @@ class DotInstaller:
 
     def install_dot(self):
         try:
+            raise Exception("die")
             self.info("Making sure virtualenv is not active")
             # self._check_virtual_env()
 
