a72146fcbf7b5c100601d7a8ef72582220b2f899
Tucker Beck <tucker.beck@gmail.com>
2022-04-25 10:23:34 -0700

    more updates to install-prereqs

--- install-prereqs.sh
+++ install-prereqs.sh
@@ -1,13 +1,36 @@
+#!/usr/bin/env bash
+
 set -e
+
+sudo grep $USER /etc/sudoers > /dev/null 2>&1
+if (( $? ))
+then
+    echo "Making passwordless sudo"
+    echo "$USER ALL=(ALL) NOPASSWD: ALL" | (sudo su -c 'EDITOR="tee" visudo')
+fi
+
 
 pyenv_root=$HOME/.pyenv
 if [[ -e $pyenv_root ]]
 then
-    debug_out "pyenv is already installed. Skipping"
+    echo "pyenv is already installed. Skipping"
 else
+    echo "Installing needed libs"
+    sudo apt-get update -y
+    sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
+    libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+    libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
     echo "Installing pyenv"
     curl https://pyenv.run | bash
+
+    export PYENV_ROOT="$HOME/.pyenv"
+    export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init --path)"
 fi
+
+echo "Installing python 3.6.12 via pyenv"
+pyenv install 3.6.15
 
 echo "Installing python 3.8.12 via pyenv"
 pyenv install 3.8.12
@@ -22,4 +45,8 @@ then
 else
     echo "Installing poetry"
     curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+    source $HOME/.poetry/env
 fi
+
+echo "Setting up neovim"
+sudo apt install neovim
