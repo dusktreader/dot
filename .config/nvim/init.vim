cbacdebcfbe0bd860fe30b544f4e66d9131f7cc9
Tucker Beck <tucker.beck@gmail.com>
2024-10-23 17:31:55 -0700

    Stuff and broke clipboard sync?

--- .config/nvim/init.vim
+++ .config/nvim/init.vim
@@ -1,126 +1,13 @@
 lua << EOF
-  require("config.lazy")
-  require("config.lualine")
-  require("config.telescope")
-  require("config.lsp")
-  require("config.cmp")
-  require("config.keymap")
-  require("config.neotest")
-  require("config.treesj")
-  require("config.tokyonight")
+  require("config.init")
 EOF
 
-" Make sure the backup directory exists correctly
-let g:backupdir=expand(stdpath('data') . '/backup')
-if !isdirectory(g:backupdir)
-   call mkdir(g:backupdir, "p")
-endif
-let &backupdir=g:backupdir
-
-lua << EOF
-  -- Sets the leader character for commands
-  vim.mapleader=","
-
-  -- Tells neovim to use indentation based on filetype
-  vim.filetype.indent = true
-
-  -- Doesn't seem to do anything any more...
-  -- make bell visual only
-  -- vim.o.visualbell = true
-
-  -- Turn off vi compatibility mode
-  vim.o.compatible = false
-
-  -- Disable wrapping by default
-  vim.o.wrap = false
-
-  -- Set the colorscheme
-  vim.api.nvim_cmd(
-    {
-      cmd = "colorscheme",
-      args = {"tokyonight"},
-    },
-    {}
-  )
-EOF
-
-"
-" Maintain undo history between sessions
-set undofile
-set undodir=~/.local/share/nvim/undodir
-
-" remove trailing whitespace right before writing file
-autocmd bufwritepre  * :%s/\s\+$//e
-
-" Shows line numbers
-set number
-
-" Tells vim to always show a status line
-set laststatus=2
-
-" Makes backspace behave the way you would expect
-set backspace=indent,eol,start
-
-" Sets scroll offsets to keep the cursor away from the edge of the screen
-set scrolloff=3
-set sidescrolloff=10
-
-" Keeps the cursor in the current column when jumping to other lines
-set nostartofline
-
-" Shows an underscore where there are trailing whitespaces
-set list listchars=trail:_
-
-" Makes vim remember the last 10000 commands
-set history=10000
-
-" Don't give the attention message when an existing swap file is found
-set shortmess+=A
-
-" highlights all matches for a search
-set hlsearch
-
-" Enables the mouse in all modes
-set mouse=a
-
-" Show the first match to a search as you type it
-set incsearch
-
-" Make the background color dark
-set background=dark
-
+" Don't think this is necessary any more
 " Enables smart indentation for newlines (without dedenting # lines)
 " see: http://vim.wikia.com/wiki/Restoring_indent_after_typing_hash
-set cindent
-set cinkeys-=0#
-set indentkeys-=0#
-
-" Put backup files and swap files in a sane place
-set backup
-set writebackup
-set backupdir=~/.local/share/nvim/backup/
-set directory=~/.local/share/nvim/swap/
-
-" Puts in column markers based on project's config (if exists)
-set termguicolors
-let g:line_length=trim(system('get-config-line-length'))
-execute "set colorcolumn=" . join(range(g:line_length + 1,335), ',')
-highlight ColorColumn ctermbg=0 guibg=#333333
-highlight EndOfBuffer ctermbg=0 guibg=#333333
-highlight PMenu ctermbg=0 guibg=DarkSlateGray
-highlight PMenuSel ctermbg=0 guibg=DarkBlue
-
-" Makes vim uses the right number of spaces instead of a tab
-set tabstop=4
-set shiftwidth=4
-set expandtab
-set smarttab
-autocmd FileType html       setlocal tabstop=2 softtabstop=2 shiftwidth=2 expandtab smarttab
-autocmd FileType javascript setlocal tabstop=2 softtabstop=2 shiftwidth=2 expandtab smarttab
-autocmd FileType typescript setlocal tabstop=2 softtabstop=2 shiftwidth=2 expandtab smarttab
-autocmd FileType yaml       setlocal tabstop=2 softtabstop=2 shiftwidth=2 expandtab smarttab
-autocmd FileType yml        setlocal tabstop=2 softtabstop=2 shiftwidth=2 expandtab smarttab
-autocmd FileType lua        setlocal tabstop=2 softtabstop=2 shiftwidth=2 expandtab smarttab
+" set cindent
+" set cinkeys-=0#
+" set indentkeys-=0#
 
 " Cursor settings for windows terminal
 if &term =~ '^xterm'
---
+++ .config/nvim/lua/config/init.lua
@@ -0,0 +1,165 @@
+require("config.lazy")
+require("config.lualine")
+require("config.telescope")
+require("config.lsp")
+require("config.cmp")
+require("config.keymap")
+require("config.neotest")
+require("config.treesj")
+require("config.tokyonight")
+
+-- Sets the leader character for commands
+vim.mapleader=","
+
+-- Tells neovim to use indentation based on filetype
+vim.filetype.indent = true
+
+-- Doesn't seem to do anything any more...
+-- make bell visual only
+-- vim.o.visualbell = true
+
+-- Turn off vi compatibility mode
+vim.o.compatible = false
+
+-- Disable wrapping by default
+vim.o.wrap = false
+
+-- Show line numbers
+vim.o.number = true
+
+-- Maintain undo history between sessions
+vim.o.undofile = true
+vim.o.undodir = "~/.local/share/nvim/undodir"
+
+-- Always show a status line
+vim.opt.laststatus = 2
+
+-- Makes backspace behave the way you would expect
+vim.o.backspace = "indent,eol,start"
+
+-- Keep the cursor away from the edge of the screen
+vim.o.scrolloff = 3
+vim.o.sidescrolloff = 10
+
+-- Keep the cursor in the current column when jumping to other lines
+vim.o.startofline = false
+
+-- Show trailing spaces, tabs, non-breakable space characters
+vim.opt.list = true
+
+-- Show fancy characters for tab and trailing whitespace
+vim.opt.listchars = {
+    tab = " ",
+    trail = ""
+}
+
+-- Remember the last 10000 commands
+vim.opt.history = 10000
+
+-- Don't give the attention message when an existing swap file is found
+vim.opt.shortmess:append("A")
+
+-- Highlight all matches for a search
+vim.opt.hlsearch = true
+
+-- Enable the mouse in all modes
+vim.opt.mouse = "a"
+
+-- Show the first match to a search as you type it
+vim.opt.incsearch = true
+
+-- Put backup files and swap files in a sane place
+vim.opt.backup = true
+vim.opt.writebackup = true
+vim.opt.backupdir = "~/.local/share/nvim/backup/"
+vim.opt.directory = "~/.local/share/nvim/swap/"
+
+-- Use the right number of spaces instead of a tab
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+
+-- Set the colorscheme
+vim.api.nvim_cmd(
+  {
+    cmd = "colorscheme",
+    args = {"tokyonight"},
+  },
+  {}
+)
+
+-- Remove all trailing whitespace on write
+vim.api.nvim_create_autocmd(
+  "BufWritePre",
+  {
+    pattern = {"*"},
+    command = ":%s/\\s\\+$//e",
+  }
+)
+
+-- Set tabstops for specified file types
+vim.api.nvim_create_autocmd(
+  "FileType",
+  {
+    pattern = {"lua", "html", "javascript", "typescript", "yaml", "yml"},
+    callback = function()
+      vim.opt_local.tabstop = 2
+      vim.opt_local.softtabstop = 2
+      vim.opt_local.shiftwidth = 2
+      vim.opt_local.expandtab = true
+      vim.opt_local.smarttab = true
+    end
+  }
+)
+
+local setLineLength = function()
+  local result = vim.system({"get-config-line-length"}, { text = true }):wait()
+  local line_length = vim.trim(result["stdout"])
+  local range = {}
+  for i=line_length + 1, 1335 do
+    table.insert(range, i)
+  end
+  local columns = table.concat(range, ",")
+  vim.opt.colorcolumn=columns
+end
+setLineLength()
+
+local hasWsl = function()
+  local result = vim.system({"cat", "/proc/version"}, { text = true }):wait()
+  local version_info = string.lower(vim.trim(result["stdout"]))
+  local i, _ = string.find(version_info, "wsl2")
+  if i == nil then
+    return false
+  end
+  return true
+end
+
+if hasWsl() then
+  -- If in wsl, do NOT check has('clipboard') and just set things
+  -- See: https://github.com/neovim/neovim/issues/8017
+  --
+  -- This might be an interesting alternative: https://stackoverflow.com/a/76388417
+
+  -- vim.opt.clipboard:append "unnamedplus"
+  -- vim.g.clipboard = {
+  --   name = 'WslClipboard',
+  --   copy = {
+  --      ["+"] = 'clip.exe',
+  --      ["*"] = 'clip.exe',
+  --   },
+  --   paste = {
+  --      ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+  --      ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+  --   },
+  --   cache_enabled = 0,
+  -- }
+else
+  -- if vim.fn.has("clipboard") then
+  --     vim.opt.clipboard = "unnamed"
+  --     if vim.fn.has("xterm_clipboard") then
+  --         vim.opt.clipboard = "unnamedplus"
+  --     end
+  -- end
+end
+
