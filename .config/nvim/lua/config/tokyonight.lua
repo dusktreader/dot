b97aef6bfdd525ec71f8b3b763998930a031eb0b
Tucker Beck <tucker.beck@gmail.com>
2025-03-18 09:44:20 -0700

    added dcw and border for neovim

--- .config/nvim/lua/config/tokyonight.lua
+++ .config/nvim/lua/config/tokyonight.lua
@@ -2,5 +2,6 @@ require("tokyonight").setup({
   style = "night",
   on_highlights = function(hl, colors)
     hl.Visual = { bg = colors.dark5 }
+    hl.WinSeparator = { fg = colors.border_highlight }
   end
 })
