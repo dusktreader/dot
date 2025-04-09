c0b417477291c64c78fab6019c24e054a2655464
Tucker Beck <tucker.beck@gmail.com>
2025-04-08 19:09:22 -0700

    added nvim telescope ui picker and updated installer

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,13 +2,24 @@ return {
   -- Fuzzy finder in neovim
   "nvim-telescope/telescope.nvim",
   tag = "0.1.8",
+  dependencies = {
+    -- Enable telescope ui for select contexts
+    { "nvim-telescope/telescope-ui-select.nvim" },
+  },
   opts = {
     pickers = {
-        find_files = {
-            hidden = true,
-            -- this would require tlescope.utils
-            -- cwd = utils.buffer_dir(),
-        }
-    }
-  }
+      find_files = {
+        hidden = true,
+        -- this would require tlescope.utils
+        -- cwd = utils.buffer_dir(),
+      },
+    },
+   extensions = {
+      ["ui-select"] = {},
+    },
+  },
+  config = function(_, opts)
+    require("telescope").setup(opts)
+    require("telescope").load_extension("ui-select")
+  end,
 }
--- install.sh
+++ install.sh
@@ -300,10 +300,15 @@ gh --version > /dev/null 2>&1
 if (( $? ))
 then
     status "Setting up github cli"
-    pushd /tmp && \
-    wget https://github.com/cli/cli/releases/download/v2.69.0/gh_2.69.0_linux_amd64.deb && \
-    sudo apt install -y ./gh_2.69.0_linux_amd64.deb && \
-    popd
+    (type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) && \
+	  sudo mkdir -p -m 755 /etc/apt/keyrings && \
+      out=$(mktemp) && \
+      wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg && \
+      cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null && \
+	  sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg && \
+	  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null && \
+	  sudo apt update && \
+	  sudo apt install gh -y
     if (( $? ))
     then
         fail "Failed to install github cli"
