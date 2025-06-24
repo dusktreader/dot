3fbe6aa96ba022b654b12256ef3d1b082ed2436d
Tucker Beck <tucker.beck@gmail.com>
2025-06-23 17:02:43 -0700

    interim commit

--- src/dot_tools/spinner.py
+++ src/dot_tools/spinner.py
@@ -1,17 +1,43 @@
+from collections import deque
 import sys
 from contextlib import contextmanager
+from time import sleep
+from typing import Any, Generator, override
 
 from loguru import logger
+from rich.console import RenderableType
 from rich.progress import Progress, SpinnerColumn, TextColumn
 from rich.live import Live
 from rich.tree import Tree
 from rich.panel import Panel
 
 branch_stack: list[Tree] = []
+logger_stack: list[int] = []
+
+class Fucky(Progress):
+
+    messages: deque[str]
+
+    def __init__(self, *args: Any, **kwargs: Any):
+        self.messages = deque(maxlen=10)
+        super().__init__(*args, **kwargs)
+
+    @override
+    def get_renderables(self):
+        for r in super().get_renderables():
+            yield r
+        for line in self.messages:
+            yield line
+
+    def fuck(self, message: str):
+        self.messages.append(message)
+
+
+last_logger: int | None = None
 
 @contextmanager
 def spinner(text: str):
-    progress = Progress(SpinnerColumn(), TextColumn("[progress.description]{task.description}"))
+    progress = Fucky(SpinnerColumn(), TextColumn("[progress.description]{task.description}"))
     progress.add_task(text)
 
     live: Live | None = None
@@ -23,7 +49,17 @@ def spinner(text: str):
     else:
         branch_stack.append(branch_stack[-1].add(progress))
 
+    global last_logger
+    if last_logger is not None:
+        logger.remove(last_logger)
+
+    last_logger = logger.add(progress.fuck)
+
     yield
+
+    if last_logger is not None:
+        logger.remove(last_logger)
+        last_logger = None
 
     branch_stack.pop()
     if len(branch_stack) > 0:
